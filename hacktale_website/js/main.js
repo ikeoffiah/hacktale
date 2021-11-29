27d058c0a14d6b5567dbf9672ecdfc0a7a92f572
IKEOFFIAH PIUS <31507347+ikeoffiah@users.noreply.github.com>
2021-11-29 21:55:20 +0100

    made the site responsive

--- hacktale_website/js/main.js
+++ hacktale_website/js/main.js
@@ -20,6 +20,76 @@ function coursePreviewVideo(){
 }
 coursePreviewVideo();
 
+ /* ------------------------------------
+header menu
+ --------------------------------------*/
+ function headerMenu(){
+     
+    const menu = document.querySelector(".js-header-menu"),
+    backdrop = document.querySelector(".js-header-backdrop"),
+    menuCollapseBreakpoint= 991;
+
+//toggle the sidebar
+    function toggleMenu(){
+        menu.classList.toggle("open");
+        backdrop.classList.toggle("active");
+        document.body.classList.toggle("overflow-hidden");
+    }
+
+    document.querySelectorAll(".js-header-menu-toggler").forEach((element) => {
+    element.addEventListener('click', toggleMenu); 
+    });
+
+// close the menu by clicking outside of it
+
+    backdrop.addEventListener("click", toggleMenu);
+
+
+    function collapse(){
+        
+        menu.querySelector(".active .js-sub-menu").removeAttribute("style");
+        menu.querySelector(".active").classList.remove("active");
+    }
+
+    menu.addEventListener("click", (event) => {
+        const { target } = event;
+
+
+        if(target.classList.contains("js-toggle-sub-menu") && window.innerWidth <= menuCollapseBreakpoint){
+            //prevent default anchor click behavior
+            event.preventDefault();
+
+            //if menu-item already expanded, collapse it and exit
+            if(target.parentElement.classList.contains("active")){
+                collapse();
+                return;
+            }
+
+            //collapse the other expanded menu-item if exists
+            if(menu.querySelector(".active")){
+                
+                collapse();
  
+            }
+
+            //expand menu item
+            target.parentElement.classList.add("active");
+            target.nextElementSibling.style.maxHeight = target.nextElementSibling.scrollHeight + "px";
+            
+            window.addEventListener("resize", function(){
+                if(this.innerWidth > menuCollapseBreakpoint && menu.classList.contains("open")){
+                    toggleMenu();
+                }
+
+                if(this.innerWidth > menuCollapseBreakpoint && menu.querySelector(".active")){
+                    collapse()
+                }
+            })
+
+        }
+    })
+
+}
+ headerMenu()
 
      
